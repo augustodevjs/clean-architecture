bb825f45d2bae8449f4b44dcb78a9ac5feaa8a29
augusto <augustosaaude@gmail.com>
2023-11-09 14:46:41 -0300

    feat: create test of list customer and implementation of it

--- src/api/routes/customer.route.ts
+++ src/api/routes/customer.route.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from 'express'
 
 import { CustomerRepository } from '../../infra';
-import { CreateCustomerUseCase } from '../../application';
+import { CreateCustomerUseCase, ListCustomerUseCase } from '../../application';
 
 export const CustomerRoute = express.Router();
 
+const repository = new CustomerRepository()
+
 CustomerRoute.post('/', async (request: Request, response: Response) => {
-  const usecase = new CreateCustomerUseCase(new CustomerRepository());
+  const usecase = new CreateCustomerUseCase(repository);
 
   try {
     const customerInputModel = {
@@ -22,6 +24,18 @@ CustomerRoute.post('/', async (request: Request, response: Response) => {
     const customerViewModel = await usecase.execute(customerInputModel);
 
     response.status(201).send(customerViewModel);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+})
+
+CustomerRoute.get('/', async (request: Request, response: Response) => {
+  const usecase = new ListCustomerUseCase(repository);
+
+  try {
+    const listCustomerViewModel = await usecase.execute();
+
+    response.status(200).send(listCustomerViewModel);
   } catch (error) {
     response.status(500).send(error);
   }
--- src/tests/e2e/customer-e2e.spec.ts
+++ src/tests/e2e/customer-e2e.spec.ts
@@ -27,4 +27,27 @@ describe("E2E test for customer", () => {
     expect(response.body.name).toEqual("John Doe");
     expect(response.body.address).toEqual(address);
   });
+
+  it("should not create a customer", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({ name: "John" });
+
+    expect(response.status).toEqual(500);
+  });
+
+  it("should list all customers", async () => {
+    await request(app).post("/customer").send({ name: "John Doe", address });
+    await request(app).post("/customer").send({ name: "Jane Doe", address });
+
+    const response = await request(app).get("/customer").send();
+    const customer1 = response.body.customers[0];
+    const customer2 = response.body.customers[1];
+
+    expect(response.status).toEqual(200);
+    expect(response.body.customers.length).toEqual(2);
+
+    expect(customer1.name).toEqual("John Doe");
+    expect(customer2.name).toEqual("Jane Doe");
+  });
 });
